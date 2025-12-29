dac1b2efe3fd2d611edd30c0acba76b635efe073
Scott Wehrwein <scott.wehrwein@wwu.edu>
2025-12-29 13:56:19 -0800

    L01

---
+++ src/filtering.py
@@ -0,0 +1,12 @@
+import numpy as np
+import imageio
+
+def brightness(img, scale):
+    """ Change the brightness of img by scaling its pixel
+    values uniformly by scale """
+    # todo
+
+def threshold(img, thresh):
+    """ Threshold img such that the output is
+    1 if the input is >= t, and 0 otherwise. """
+    # todo
---
+++ src/util.py
@@ -0,0 +1,29 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage as skim
+
+
+def imread_grayfloat(path):
+    return skim.color.rgb2gray(byte2float(imageio.imread(path)))
+
+def byte2float(img):
+    """ Convert an image from bytes (0-255) to floats (0.0-1.0) """
+    return img.astype(np.float32) / 255.0
+
+def imshow_gray(img):
+    assert len(img.shape) == 2
+    plt.imshow(img, cmap="gray")
+
+def imshow_truesize(img):
+    dpi = plt.rcParams['figure.dpi']
+    height, width = img.shape[:2]
+    figsize = width / float(dpi), height / float(dpi)
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.gca()
+    ax.set_axis_off()
+    fig.subplots_adjust(0,0,1,1)
+    plt.imshow(img, cmap="gray", interpolation="none")
+
+    
