440288f9083192119f5bb4a9e4806cbe17e7474c
Scott Wehrwein <scott.wehrwein@wwu.edu>
2026-01-05 09:24:16 -0800

    Post L01

---
+++ notebooks/L01B_md_sample.md
@@ -0,0 +1,19 @@
+# Big Heading
+## Next heading
+
+This is **markdown**.
+
+1. numbered
+2. lists
+
+* bulleted lists
+* and stuff
+
+
+
+Inline math: $x + y$
+
+Math block:
+$$
+ax + by + c = 0
+$$
--- src/filtering.py
+++ src/filtering.py
@@ -4,9 +4,10 @@ import imageio
 def brightness(img, scale):
     """ Change the brightness of img by scaling its pixel
     values uniformly by scale """
-    # todo
+    return np.clip(img * scale, 0, 1)
 
 def threshold(img, thresh):
     """ Threshold img such that the output is
-    1 if the input is >= t, and 0 otherwise. """
-    # todo
+    1 if the input is >= thresh, and 0 otherwise. 
+    Pre: img is grayscale """
+    return img >= thresh
